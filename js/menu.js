a149c4073bf1c340dc5755e01b0d2518fb002519
scottyyyyy <32375007+scottyyyyy@users.noreply.github.com>
2021-05-28 22:48:17 +0100

    added js to starters

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+function myStarters() {
+  var x = document.getElementById("starters");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
