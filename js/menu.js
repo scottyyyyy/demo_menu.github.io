c7dce0ad87e4387f171316586a36e9a5eab20cf5
scottyyyyy <32375007+scottyyyyy@users.noreply.github.com>
2021-05-28 23:09:17 +0100

    added js for mains

--- js/menu.js
+++ js/menu.js
@@ -6,3 +6,21 @@ function myStarters() {
     x.style.display = "none";
   }
 }
+
+function myPasta() {
+  var x = document.getElementById("pasta");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myMains() {
+  var x = document.getElementById("mains");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
